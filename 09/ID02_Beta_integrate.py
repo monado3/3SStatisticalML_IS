a9e7820409d772804367937f7d1ddc3ab305469c
monado3 <monade.kk@gmail.com>
2018-06-28 15:59:06 +0900

    minor change

--- 09/ID02_Beta_integrate.py
+++ 09/ID02_Beta_integrate.py
@@ -6,9 +6,9 @@ from scipy.stats import beta
 n = int(1e4)
 for a, b in (1, 1), (0.1, 0.1), (5, 5):
     rv = beta(4 + a, 1 + b)
+    print(f'prior distribution     : Beta({a},{b})')
+    print(f'posterior distribution : Beta({4+a},{1+b})')
     for start in 0.5, 0.8:
         x = np.linspace(start, 1, n)
-    print(f'prior distribution     : Beta({a},{b})',
-          f'posterior distribution : Beta({4+a},{1+b})',
-          f'prob(pi >= {start}|data)   : {integrate.simps(rv.pdf(x), x):.5f}',
-          sep='\n', end='\n\n')
+        print(f'prob(pi >= {start}|data)   : {integrate.simps(rv.pdf(x), x):.5f}')
+    print('')
--- 09/ID03_Beta_integrate.py
+++ 09/ID03_Beta_integrate.py
@@ -10,13 +10,14 @@ for a, b in (1, 1), (0.1, 0.1), (5, 5):
     print(f'prior distribution     : Beta({a},{b})')
     print(f'posterior distribution : Beta({5+a},{15+b})')
 
+
     # E[x|data]
     def E_integrand(pi):
         return 2 * ((1 - pi) / pi) * rv.pdf(pi)
 
-    v_E_integrand = np.vectorize(E_integrand)
     pi_arr = np.linspace(1e-10, 1, n)
-    print(f'E[x|data]              : {integrate.simps(v_E_integrand(pi_arr), pi_arr):.5f}')
+    print(f'E[x|data]              : {integrate.simps(E_integrand(pi_arr), pi_arr):.5f}')
+
 
     # Pr[pi <=  threshold|data]
     pi_arr = np.linspace(0, threshold, n)
